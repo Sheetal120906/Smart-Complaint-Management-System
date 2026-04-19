8d821afdc00b5f0798fa6f2c9ea7571293a55cd6
Sheetal Nimbarte <sheetalnimbarte55@gmail.com>
2026-04-19 16:37:36 +0530

    updated project with filter feature and env setup

--- db.js
+++ db.js
@@ -1,17 +1,19 @@
+require("dotenv").config();   
+
 const mysql = require("mysql2");
 
-const db = require("mysql2").createConnection({
-    host: "localhost",
-    user: "root",
-    password: "Savn@60029021",   //  EXACT same password you entered in terminal
-    database: "complain_system"
+const db = mysql.createConnection({
+    host: process.env.DB_HOST,
+    user: process.env.DB_USER,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_NAME
 });
 
-db.connect((err) => {
+db.connect(err => {
     if (err) {
-        console.log(" DB ERROR:", err.message);
+        console.error("DB Connection Failed:", err);
     } else {
-        console.log("MySQL Connected ");
+        console.log("Connected to MySQL");
     }
 });
 
